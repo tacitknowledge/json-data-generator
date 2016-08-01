d33df129fb8bbacbdc1060cd22684fd93cf629e1
Hyun Jeong <hjeong@tacitknowledge.com>
2016-08-01 10:18:51 -0700

    use more objected oriented approach and optionally specify collection size

--- README.md
+++ README.md
@@ -6,10 +6,10 @@ Uses Faker.js (https://github.com/Marak/faker.js) to generate mock data.
 
 ## Usage
 1. Install Node and NPM
-2. run `node generate.js sample.json.template [outputfile]`
+2. run `node generate.js sample.json.template [-o outputfile] [-c collectionCount ]`
 
 ## Template Syntax
 1. Anything wrapped in `${ ... }` will be evaluated.  Works with any javascript syntax
 2. To use Faker API, use `${ faker. ...}`. See http://marak.github.io/faker.js/index.html for available faker documentation
 3. You can specify random number of elements in an array by using the following syntax as the property name: `<property name>__$repeat_<min>_<max>`. See `sample.json.template`
-
+4. If -c is specified, it will create a collection of the template objects
--- generate.js
+++ generate.js
@@ -1,9 +1,11 @@
 const fs = require('fs');
 const faker = require('faker');
+const argv = require('minimist')(process.argv.slice(2));
 
-const args = process.argv.slice(2);
-const template = args[0];
-const outputFile = args[1];
+// const args = process.argv.slice(2);
+ const template = argv._[0];
+  const collectionSize = argv.c;
+ const outputFile = argv.o;
 
 class EvalNode {
   constructor (parent, nodeKey, expr) {
@@ -13,91 +15,109 @@ class EvalNode {
   }
 }
 
-let nodesToEvaluate = [];
-
-let $root = JSON.parse(fs.readFileSync(template, 'utf8'));
-
-function alphaNumeric(min,max){
-  let count = faker.random.number({max: max, min: min});
-  let returnVal = "";
-  for(var i=0; i<count; i++){
-    returnVal += faker.random.alphaNumeric();
+class Generator {
+  constructor (temp) {
+    this.nodesToEvaluate = [];
+    this.$root = JSON.parse(fs.readFileSync(temp, 'utf8'));
   }
-  return returnVal.toUpperCase();
-}
 
-function traverse (jsonObj, path, parent) {
-  if (typeof jsonObj == "object") {
-    Object.keys(jsonObj).forEach((key) => {
-      let realKey = key;
-      if(Array.isArray(jsonObj[key])){
-        realKey = key.split("__")[0];
-        let repeat = key.split("__").pop().split("_");
-        if(repeat.length === 3 && repeat[0] === "$repeat"){
-          const min = parseInt(repeat[1]);
-          const max = parseInt(repeat[2]);
-          let count = faker.random.number({max: max, min: min});
-          jsonObj[realKey] = [];
-          while (count > 0){
-            jsonObj[realKey].push(Object.assign({}, jsonObj[key][0]));
-            count--;
+  static alphaNumeric (min, max) {
+    let count = faker.random.number({max: max, min: min});
+    let returnVal = "";
+    for (var i = 0; i < count; i++) {
+      returnVal += faker.random.alphaNumeric();
+    }
+    return returnVal.toUpperCase();
+  }
+
+  traverse (jsonObj, path, parent) {
+    if (typeof jsonObj == "object") {
+      Object.keys(jsonObj).forEach((key) => {
+        let realKey = key;
+        if (Array.isArray(jsonObj[key])) {
+          realKey = key.split("__")[0];
+          let repeat = key.split("__").pop().split("_");
+          if (repeat.length === 3 && repeat[0] === "$repeat") {
+            const min = parseInt(repeat[1]);
+            const max = parseInt(repeat[2]);
+            let count = faker.random.number({max: max, min: min});
+            jsonObj[realKey] = [];
+            while (count > 0) {
+              jsonObj[realKey].push(Object.assign({}, jsonObj[key][0]));
+              count--;
+            }
+            delete jsonObj[key];
           }
-          delete jsonObj[key];
         }
-      }
-      path.push(realKey);
-      traverse(jsonObj[realKey], path, jsonObj);
-    })
-  }
-  else {
-    let value = jsonObj;
-    if (typeof value === "string" && value.trim().match(/\${.+}/)) {
-      value = value.trim();
-      const match = value.match(/\${(.+)}/);
-      let result = null;
-      let expr = null;
-      if(match.length > 1){
-        expr = match[1]
-      }
-      result = eval(expr);
-      const key = path[path.length - 1];
-      if ((typeof result === "string") && result.match(/\${.+}/)) {
-        nodesToEvaluate.push(new EvalNode(parent, key, expr))
-      }
-      else {
-        parent[path[path.length - 1]] = result
+        path.push(realKey);
+        this.traverse(jsonObj[realKey], path, jsonObj);
+      })
+    }
+    else {
+      let value = jsonObj;
+      if (typeof value === "string" && value.trim().match(/\${.+}/)) {
+        value = value.trim();
+        const match = value.match(/\${(.+)}/);
+        let result = null;
+        let expr = null;
+        if (match.length > 1) {
+          expr = match[1]
+        }
+        result = eval(expr);
+        const key = path[path.length - 1];
+        if ((typeof result === "string") && result.match(/\${.+}/)) {
+          this.nodesToEvaluate.push(new EvalNode(parent, key, expr))
+        }
+        else {
+          parent[path[path.length - 1]] = result
+        }
       }
     }
   }
-}
 
-function evaluateAllVAlues(rootObj, nodesToEvaluate) {
-  while (nodesToEvaluate.length > 0) {
-    nodesToEvaluate = nodesToEvaluate.filter((evalNode)=>{
-      let $root = rootObj;
-      const result = eval(evalNode.expr);
-      if (!result.match(/\${.+}/)) {
-        evalNode.parent[evalNode.nodeKey] = result;
-        return false
-      }
-      else {
-        return true
-      }
-    });
+  evaluateAllValues () {
+    while (this.nodesToEvaluate.length > 0) {
+      this.nodesToEvaluate = this.nodesToEvaluate.filter((evalNode)=> {
+        const result = eval(evalNode.expr);
+        if (!result.match(/\${.+}/)) {
+          evalNode.parent[evalNode.nodeKey] = result;
+          return false
+        }
+        else {
+          return true
+        }
+      });
+    }
+  }
+
+  generate () {
+    this.traverse(this.$root, ['$root'], null);
+    this.evaluateAllValues(this.$root, this.nodesToEvaluate);
+    return this.$root;
+    //this.writeOutput(this.outFile)
   }
 }
 
-function writeOutput(outputFile) {
-  if(outputFile){
-    fs.writeFileSync(outputFile, JSON.stringify($root, null, 2));
+function writeOutput (jsonObj, outputFile) {
+  if (outputFile) {
+    fs.writeFileSync(outputFile, JSON.stringify(jsonObj, null, 2));
   }
   else {
-    console.log($root);
+    console.log(jsonObj);
   }
 }
 
-traverse($root, ['$root'], null);
-evaluateAllVAlues($root, nodesToEvaluate);
-writeOutput(outputFile)
-
+if(Number.isInteger(collectionSize)){
+  const count = parseInt(collectionSize);
+  let collection = [];
+  for (let i=0; i<count; i++) {
+    let generator = new Generator(template);
+    collection.push(generator.generate())
+  }
+  writeOutput(collection, outputFile)
+}
+else {
+  let generator = new Generator(template);
+  writeOutput(generator.generate(), outputFile)
+}
 
